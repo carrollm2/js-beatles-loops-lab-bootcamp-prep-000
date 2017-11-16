cb1ace4018398d159e31d95df5b7df5b36f1226d
Michael Carroll <carrollm2@gmail.com>
2017-11-16 15:28:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments){
   var array = []
 
   for (var i = 0; i < musicians.length; i++){
-      array.unshift(`${musicians[i]}` + " plays the " + `${instruments[i]}`)
+      array.push(`${musicians[i]}` + " plays the " + `${instruments[i]}`)
   }
 
   return array
