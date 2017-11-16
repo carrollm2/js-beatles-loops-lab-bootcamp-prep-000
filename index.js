ab3abfebaa4e2c79a2058247106073f9e475c547
Michael Carroll <carrollm2@gmail.com>
2017-11-16 15:33:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts){
   var i = 0;
   var array = []
-  
+
   while(i < facts.length){
     array.push(`{facts[i]}` + "!!!")
     i--;
